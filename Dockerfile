c87c2575157bf874238fbc15a74c157054acbfbd
Askaredox <andyecarvajal@gmail.com>
2020-11-06 19:07:48 -0600

    [fix] cambio de arquitectura

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3.8
-ADD . /codeSERVE
-WORKDIR /codeSERVE
-RUN pip install -r requirements.txt
-CMD python app.py
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Antes de correr
-## chmod +x start.sh
-
-cd uSocial-backend
-sudo docker-compose -f "docker-compose.yaml" up -d --build
-sudo docker ps
-echo Está listo!
